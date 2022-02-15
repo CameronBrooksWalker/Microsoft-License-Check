7f97bc73936d447b8c9d56fc55d0ce50a66ee1b3
Cameron Walker <cameronbrookswalker@gmail.com>
2022-02-15 11:51:10 -0600

    Update checktivation.ps1

--- checktivation.ps1
+++ checktivation.ps1
@@ -1,21 +1,7 @@
 
 
-$computers = Get-Content "C:\Users\cwalker\Desktop\allcomputers.txt"
+$alldomain  =  get-adcomputer  -filter *  -property *
 
-try {
+$computers  =  $alldomain.CN
 
-Get-WmiObject SoftwareLicensingProduct -ComputerName $computers | where {$_.PartialProductKey} | Select-Object pscomputername, Name, productkeychannel, licensestatus | Export-Csv -path c:\users\cwalker\desktop\arrrgmatey3000.csv -notypeinformation -ErrorAction Stop
-
-} Catch { $_| Out-File c:\users\cwalker\licensingerrorresponses.txt } #not currently working plz fix
-
-<#
-
-0   Unlicensed
-1   Licensed
-2   OOBGrace
-3   OOTGrace
-4   NonGenuineGrace
-5   Notification
-6   ExtendedGrace
-
-#>
+Get-WmiObject SoftwareLicensingProduct -ComputerName $computers | where {$_.PartialProductKey} | Select-Object pscomputername, Name, productkeychannel, licensestatus | Export-Csv -path .\licensecheck.csv -notypeinformation -ErrorAction Stop
